9f94a3b6d07e4581fa8c0aec61410561da7d3399
AntonC9018 <anton.curm@gmail.com>
2023-03-11 12:42:35 +0200

    linear regression

---
+++ linear_regression/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 100
+xs = np.linspace(0, 1, num=n) + 0.3 * np.random.uniform(-1, 1, n)
+ys = np.linspace(0, 1, num=n)
+
+xy_sum = (xs * ys).sum()
+x_sum = xs.sum()
+y_sum = ys.sum()
+xsq_sum = (xs * xs).sum()
+slope = (n * xy_sum - x_sum * y_sum) / (n * xsq_sum - x_sum * x_sum)
+b = (y_sum - slope * x_sum) / n
+
+fig, ax = plt.subplots()
+ax.scatter(xs, ys)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.axline((0, b), slope=slope, c='#ff0000')
+plt.show()
